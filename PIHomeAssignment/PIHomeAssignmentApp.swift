5e6e7b203824be15a7acd6f8f33459d6ddd6f738
Svyat Zubyak <svyat.zubyak@gmail.com>
2022-10-13 18:54:20 +0300

    Initial Commit

---
+++ PIHomeAssignment/PIHomeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIHomeAssignmentApp.swift
+//  PIHomeAssignment
+//
+//  Created by Svyat Zubyak on 13.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PIHomeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
